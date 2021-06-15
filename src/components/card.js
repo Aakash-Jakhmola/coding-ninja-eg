76c7f9003748781e5edf33ede4f618bda164e630
Aakash Jakhmola <aakash001jakhmola@gmail.com>
2021-06-15 23:52:55 +0530

    added box in img

--- src/components/card.js
+++ src/components/card.js
@@ -9,14 +9,19 @@ const MakeCard = (props) => {
 
   let d = new Date(0);
   d.setUTCSeconds(props.data.event_start_time);
-  let date_mod = date.format(d, 'hh:mm A, DD MMM YYYY') ;
+  let date_mod = date.format(d, 'hh:mm A, DD MMM YYYY');
   let others = props.data.registered_users.other_users_count
-  
+
+  d = new Date(0) ;
+  d.setUTCSeconds(props.data.registration_end_time)
+  let reg_end_date = date.format(d, 'hh:mm A, DD MMM YYYY');
   return <div className="Card">
+    
     <div className="imgContainer">
       <img className="img" src={props.data.cover_picture} />
+      {!props.isArchived && <div className="img-text"><p>Registrations open till {reg_end_date}</p></div>}
     </div>
-    <div className = "container-b">
+    <div className="container-b">
       <div className="textContainer">
         <div>
           <h2>{props.data.name}</h2>
@@ -30,7 +35,7 @@ const MakeCard = (props) => {
             </tr>
             <tr>
               <td>{date_mod}</td>
-              <td>{ props.data.currency + " " + props.data.fees}</td>
+              <td>{props.data.currency + " " + props.data.fees}</td>
               <td>{props.data.venue}</td>
             </tr>
           </table>
@@ -41,14 +46,20 @@ const MakeCard = (props) => {
         {props.data.card_tags.map((item) => <Tag data={item} />)}
       </div>
     </div>
-    <hr className="solid-divider"/>
-    <div className="container-b">
-      <div className="users-list">
-        {props.data.registered_users.top_users.map((item) => <Avatar imgurl = {item.image_url} name = {item.name}/>)}
-      </div>
-      <div>
-        {others > 0 && ("and " + others + " others registered")}
-        <button className="btn">Register Now</button>
+    <div className="footer">
+      <hr className="solid-divider" />
+      <div className="container-b">
+        <div className="container-c">
+          <div className="users-list">
+            {props.data.registered_users.top_users.map((item) => <Avatar imgurl={item.image_url} name={item.name} />)}
+          </div>
+          {!props.isArchived && <button className="btn">Register Now</button>}
+        </div>
+
+        <div>
+          {others > 0 && ("and " + others + " others registered")}
+
+        </div>
       </div>
     </div>
   </div>;
@@ -59,18 +70,19 @@ export default (props) => {
   let activeTags = "";
   console.log(props.list)
   props.list.forEach((s) => activeTags += s + ',')
-
-  
+  let isArchived = false ;
+  if(props.activeSubCategory === "Archived")
+    isArchived = true ;
 
   console.log(activeTags)
   let url = 'https://api.codingninjas.com/api/v3/events?';
   url += 'event_category=';
   url += props.activeCategory + '&';
   url += 'event_sub_category=';
-  url +=  props.activeSubCategory  + '&';
+  url += props.activeSubCategory + '&';
   url += 'tag_list=';
   if (activeTags.length > 0)
-    url += activeTags.slice(0, -1) ;
+    url += activeTags.slice(0, -1);
   url += '&';
   url += 'offset=';
   url += '0';
@@ -82,13 +94,13 @@ export default (props) => {
     console.log("indise");
     console.log(props.activeCategory);
     console.log(props.activeSubCategory);
-    
+
     let new_url = "";
-    for(let i = 0 ; i < url.length; ++i)  {
-      if(url[i] === ' ')
+    for (let i = 0; i < url.length; ++i) {
+      if (url[i] === ' ')
         new_url += "%20";
       else
-      new_url += url[i];
+        new_url += url[i];
     }
 
     url = new_url;
@@ -100,9 +112,9 @@ export default (props) => {
       }
         , (err) => console.log(err)
       )
-  }, [url, activeTags, ])
+  }, [url, activeTags,])
 
   return <div className="cardContainer">
-    {data && data.map((item) => <MakeCard data={item} />)}
+    {data && data.map((item) => <MakeCard isArchived = {isArchived} data={item} />)}
   </div>;
 }
